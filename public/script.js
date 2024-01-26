4bd3894efb99b08dbc33aa549eb011dc4dbbd9fc
LukHet <lukaszhetman1@gmail.com>
2024-01-26 03:20:31 +0100

    Quiz functionality and answers handling.

--- public/script.js
+++ public/script.js
@@ -3,8 +3,16 @@ const answer1 = document.querySelector("#answer1");
 const answer2 = document.querySelector("#answer2");
 const answer3 = document.querySelector("#answer3");
 const answer4 = document.querySelector("#answer4");
+const gameBoard = document.querySelector("#game-board");
+const h2 = document.querySelector("h2");
 
 const fillQuestionElements = (data) => {
+  if (data.winner === true) {
+    gameBoard.style.display = "none";
+    h2.innerText = "Wygrana!";
+    return;
+  }
+
   question.innerText = data.question;
 
   for (const i in data.answers) {
@@ -24,3 +32,29 @@ const showNextQuestion = () => {
 };
 
 showNextQuestion();
+
+const goodAnswersSpan = document.querySelector("#good-answers");
+
+const handleAnswerFeedback = (data) => {
+  goodAnswersSpan.innerText = data.goodAnswers;
+  showNextQuestion();
+};
+
+const sendAnswer = (answerIndex) => {
+  fetch(`/answer/${answerIndex}`, {
+    method: "POST",
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      handleAnswerFeedback(data);
+    });
+};
+
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", (event) => {
+    const answerIndex = event.target.dataset.answer;
+    sendAnswer(answerIndex);
+  });
+}
--- routes/game.js
+++ routes/game.js
@@ -1,5 +1,6 @@
 const gameRoutes = (app) => {
   let goodAnswers = 0;
+  let isGameOver = false;
   let callToAFriendUsed = false;
   let questionToTheCrowd = false;
   let halfOnHalfUsed = false;
@@ -8,30 +9,30 @@ const gameRoutes = (app) => {
     {
       question: "Treść pytania pierwszego",
       answers: [
-        "pierwsza odpowiedź",
-        "druga odpowiedź",
-        "trzecia odpowiedź",
-        "czwarta odpowiedź",
+        "odpowiedź 1.1",
+        "odpowiedź 1.2",
+        "odpowiedź 1.3",
+        "odpowiedź 1.4",
       ],
       correctAnswer: 1,
     },
     {
       question: "Treść pytania drugiego",
       answers: [
-        "pierwsza odpowiedź",
-        "druga odpowiedź",
-        "trzecia odpowiedź",
-        "czwarta odpowiedź",
+        "odpowiedź 2.1",
+        "odpowiedź 2.2",
+        "odpowiedź 2.3",
+        "odpowiedź 2.4",
       ],
       correctAnswer: 2,
     },
     {
       question: "Treść pytania trzeciego",
       answers: [
-        "pierwsza odpowiedź",
-        "druga odpowiedź",
-        "trzecia odpowiedź",
-        "czwarta odpowiedź",
+        "odpowiedź 3.1",
+        "odpowiedź 3.2",
+        "odpowiedź 3.3",
+        "odpowiedź 3.4",
       ],
       correctAnswer: 3,
     },
@@ -42,7 +43,12 @@ const gameRoutes = (app) => {
       res.json({ winner: true });
       return;
     }
-
+    if (isGameOver) {
+      res.json({
+        loser: true,
+      });
+      return;
+    }
     const nextQuestion = questions[goodAnswers];
 
     const { question, answers } = nextQuestion;
@@ -50,6 +56,33 @@ const gameRoutes = (app) => {
     res.json({
       question,
       answers,
+    });
+  });
+
+  app.post("/answer/:index", (req, res) => {
+    if (isGameOver) {
+      res.json({
+        loser: true,
+      });
+      return;
+    }
+    const index = req.params.index;
+
+    const question = questions[goodAnswers];
+
+    console.log(typeof question.correctAnswer, typeof Number(index));
+
+    const isGoodAnswer = question.correctAnswer === Number(index);
+
+    if (isGoodAnswer) {
+      goodAnswers++;
+    } else {
+      isGameOver = true;
+    }
+
+    res.json({
+      correct: isGoodAnswer,
+      goodAnswers,
     });
   });
 };
